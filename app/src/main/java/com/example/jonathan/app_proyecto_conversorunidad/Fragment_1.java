8a2c48b03a2f3e28d29cf4dcb09b263ff2200d6a
jonathan <jonathan.seminario@hotmail.com>
2018-04-26 14:10:30 -0500

    integracion 2- jonathan

---
+++ app/src/main/java/com/example/jonathan/app_proyecto_conversorunidad/Fragment_1.java
@@ -0,0 +1,23 @@
+package com.example.jonathan.app_proyecto_conversorunidad;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_1, container, false);
+
+
+    }
+
+
+}
