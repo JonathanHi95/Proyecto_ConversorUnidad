1f72e9d368b50d75b9956d029d84d806223e04e9
jonathan <jonathan.seminario@hotmail.com>
2018-04-30 17:23:27 -0500

    integracion 2- jonathan

--- app/src/main/java/com/example/jonathan/app_proyecto_conversorunidad/principal.java
+++ app/src/main/java/com/example/jonathan/app_proyecto_conversorunidad/principal.java
@@ -30,6 +30,7 @@ public class principal extends AppCompatActivity
     private FrameLayout frm1;
     LinearLayout l1, l2, l3;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
